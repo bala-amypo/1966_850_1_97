7cea4ddde6092a2e46b51868d1b53efd4fa1ac1a
bala-amypo <bala@amypo.in>
2025-12-20 17:16:10 +0000

    User commit

--- src/main/java/com/example/demo/entity/Certificate.java
+++ src/main/java/com/example/demo/entity/Certificate.java
@@ -1,1 +1,32 @@
 
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Certificate {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Student student;
+
+    @ManyToOne
+    private CertificateTemplate template;
+
+    private LocalDate issuedDate;
+
+    @Column(unique = true)
+    private String verificationCode;
+
+    private String qrCodeUrl;
+}
