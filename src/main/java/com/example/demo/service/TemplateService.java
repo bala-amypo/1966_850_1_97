334db9765287bee001562113fc952a9e5572ec27
bala-amypo <bala@amypo.in>
2025-12-26 09:15:06 +0000

    User commit

--- src/main/java/com/example/demo/service/TemplateService.java
+++ src/main/java/com/example/demo/service/TemplateService.java
@@ -1,12 +1,1 @@
-package com.example.demo.service;
 
-import com.example.demo.entity.CertificateTemplate;
-
-import java.util.List;
-
-public interface TemplateService {
-
-    CertificateTemplate addTemplate(CertificateTemplate template);
-
-    List<CertificateTemplate> getAllTemplates();
-}
--- src/main/java/com/example/demo/service/VerificationService.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.entity.Certificate;
-
-public interface VerificationService {
-
-    Certificate verify(String verificationCode, String ipAddress);
-}
