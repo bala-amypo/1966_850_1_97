72a09f726e64251a1417eb3dbe3115a51b688e2d
bala-amypo <bala@amypo.in>
2025-12-26 09:21:56 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/CertificateTemplate.java
@@ -0,0 +1,21 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter @Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CertificateTemplate {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String templateName;
+
+    private String backgroundUrl;
+}
