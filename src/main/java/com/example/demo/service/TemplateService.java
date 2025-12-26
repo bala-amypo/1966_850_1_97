4b1b612c02e8b57742abcc4a7c06c1bccd6080ca
bala-amypo <bala@amypo.in>
2025-12-26 09:48:20 +0000

    User commit

--- src/main/java/com/example/demo/service/TemplateService.java
+++ src/main/java/com/example/demo/service/TemplateService.java
@@ -1,1 +1,8 @@
 
+package com.example.demo.service;
+
+import com.example.demo.entity.CertificateTemplate;
+
+public interface TemplateService {
+    CertificateTemplate save(CertificateTemplate template);
+}
