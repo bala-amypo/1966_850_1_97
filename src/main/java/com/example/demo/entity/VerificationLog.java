0c6a52012e6dd6ca66476f168292c33cfd3a882e
bala-amypo <bala@amypo.in>
2025-12-26 09:37:08 +0000

    User commit

--- src/main/java/com/example/demo/entity/VerificationLog.java
+++ src/main/java/com/example/demo/entity/VerificationLog.java
@@ -6,17 +6,22 @@ import lombok.*;
 import java.time.LocalDateTime;
 
 @Entity
-@Getter @Setter
-@Builder
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
+@Table(name = "verification_logs")
 public class VerificationLog {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String verificationCode;
+
     private String status;
+
     private String ipAddress;
+
     private LocalDateTime verifiedAt;
 }
