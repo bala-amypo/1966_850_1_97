4ad676caa8b4d8cb743018e24c76f0dd7d1fd469
bala-amypo <bala@amypo.in>
2025-12-26 10:13:07 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,10 +1,11 @@
 package com.example.demo.controller;
-import com.example.demo.security.JwtUtil;
 
 import com.example.demo.dto.AuthRequest;
 import com.example.demo.dto.AuthResponse;
 import com.example.demo.dto.RegisterRequest;
 import com.example.demo.security.JwtUtil;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,14 +13,21 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     private final JwtUtil jwtUtil;
+    private final UserDetailsService userDetailsService;
 
-    public AuthController(JwtUtil jwtUtil) {
+    public AuthController(JwtUtil jwtUtil, UserDetailsService userDetailsService) {
         this.jwtUtil = jwtUtil;
+        this.userDetailsService = userDetailsService;
     }
 
     @PostMapping("/login")
     public AuthResponse login(@RequestBody AuthRequest request) {
-        String token = jwtUtil.generateToken(request.getEmail());
+
+        UserDetails userDetails =
+                userDetailsService.loadUserByUsername(request.getEmail());
+
+        String token = jwtUtil.generateToken(userDetails);
+
         return new AuthResponse(token);
     }
 
