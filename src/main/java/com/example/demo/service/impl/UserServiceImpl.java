42b86c44f6923b2f72ffe72603246132adb708d1
bala-amypo <bala@amypo.in>
2025-12-26 04:03:15 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -0,0 +1,12 @@
+package com.example.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public String getUserByUsername(String username) {
+        return username;
+    }
+}
