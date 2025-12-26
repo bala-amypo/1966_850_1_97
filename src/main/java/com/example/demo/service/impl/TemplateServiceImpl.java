0f60d81b1dfc363663053fce444e10cc4674c637
bala-amypo <bala@amypo.in>
2025-12-26 09:48:36 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/TemplateServiceImpl.java
@@ -0,0 +1,21 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.entity.CertificateTemplate;
+import com.example.demo.repository.CertificateTemplateRepository;
+import com.example.demo.service.TemplateService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TemplateServiceImpl implements TemplateService {
+
+    private final CertificateTemplateRepository repository;
+
+    public TemplateServiceImpl(CertificateTemplateRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public CertificateTemplate save(CertificateTemplate template) {
+        return repository.save(template);
+    }
+}
