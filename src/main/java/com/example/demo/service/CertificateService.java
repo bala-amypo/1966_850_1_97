e17e80b2ff4740f7f1986159bc2cd4c367f86e84
bala-amypo <bala@amypo.in>
2025-12-26 08:14:08 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/CertificateService.java
@@ -0,0 +1,16 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Certificate;
+
+import java.util.List;
+
+public interface CertificateService {
+
+    Certificate generateCertificate(Long studentId, Long templateId);
+
+    Certificate getCertificate(Long id);
+
+    Certificate findByVerificationCode(String verificationCode);
+
+    List<Certificate> findByStudentId(Long studentId);
+}
