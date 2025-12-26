02a810a4d462ee6830045e3135f06bfd11878a13
bala-amypo <bala@amypo.in>
2025-12-26 09:36:40 +0000

    User commit

--- src/main/java/com/example/demo/entity/CertificateTemplate.java
+++ src/main/java/com/example/demo/entity/CertificateTemplate.java
@@ -4,18 +4,20 @@ import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
-@Getter @Setter
-@Builder
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
+@Table(name = "certificate_templates")
 public class CertificateTemplate {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
     private String templateName;
 
-    private String backgroundUrl;
+    @Column(nullable = false)
+    private String backgroundImageUrl;
 }
